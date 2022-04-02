25be387953ca1793e82c29511300f8e9b0b5953e
Ahmad Byagowi <ahmadexp@gmail.com>
2022-04-02 14:07:28 -0700

    Update bcm54210pe_ptp.c
    use a faster bitwise operation approximation for the conversion of the ppm to 8ns 32bit adjustment register

--- drivers/net/phy/bcm54210pe_ptp.c
+++ drivers/net/phy/bcm54210pe_ptp.c
@@ -814,7 +814,9 @@ static int bcm54210pe_adjfine(struct ptp_clock_info *info, long scaled_ppm)
 
 	// This is not completely accurate but very fast
 	//scaled_ppm >>= 7;
-	scaled_ppm *=2147  //2^31 divided by millin (ppm)	
+	//scaled_ppm *=2147  //2^31 divided by millin (ppm)	
+	//or use the faster bitwise operation approx
+	scale_ppm <<= 11;
 	
 	base_8ns_interval = 1 << 31;
 
